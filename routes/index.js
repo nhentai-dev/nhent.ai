fec35949f3f217888037683b19603abca122a57f
iqbal rifai <iqbalrifai@waifu.club>
2019-03-24 17:42:59 +0700

    Update index.js
    Fix scrape, and add Meta tags

--- routes/index.js
+++ routes/index.js
@@ -4,10 +4,6 @@ var fs = require('fs');
 var request = require('request');
 var cheerio = require('cheerio');
 var app     = express();
-/* GET home page. */
-
-
-
 router.get('/g/:id',function (req,res,next) {
   var id = req.params.id;
 
@@ -16,34 +12,31 @@ router.get('/g/:id',function (req,res,next) {
     if(!err)
     {
       var $ = cheerio.load(html);
-      var a = $('#thumbnail-container .thumb-container .gallerythumb img.lazyload');
       var header = $('head');
+	  var description = $('meta[name="description"]');
+	  var name = $('meta[itemprop="name"]');
+	  var image = $('meta[itemprop="image"]');
+	  var ogtype = $('meta[property="og:type"]');
+	  var ogtitle = $('meta[property="og:title"]');
+	  var ogimage = $('meta[property="og:image"]');
+	  var twcard = $('meta[name="twitter:card"]');
+	  var twtitle = $('meta[name="twitter:title"]');
+	  var twdesc = $('meta[name="twitter:description"]');
       var data ="";
+      var title = $('title');
       $('nav').filter(function () {
         data += $(this);
       });
-      
       $('#bigcontainer').filter(function () {
         data+=$(this);
       });
-//      a.each(function (i,link) {
-//        var u = $(link).attr("data-src");
-//        console.log(u);
-//        var b = u.replace("//","").split("/");
-//        var i= b.length;
-//        var c = b[i-2];
-//        var d = b[i-1];
-//        d=d.replace("t.",".");
-//        var x = "<img src='//cdn.staticaly.com/img/i.nhentai.net/galleries/"+c+"/"+d+"' class='pages69' style='display:inline-block;'>";
-//        data+=x;
-//     });
       $('#thumbnail-container').filter(function () {
         data+=$(this);
       });
       $("#related-container").filter(function () {
         data+=$(this);
       });
-      res.render('index',{data:data,header:header});
+      res.render('index',{data:data,header:header,title:title, description:description, name:name, image:image, ogtype:ogtype, ogtitle:ogtitle, ogimage:ogimage, twcard:twcard, twdesc:twdesc, twtitle:twtitle});
     }
   });
 });
@@ -57,6 +50,16 @@ router.get('/', function(req, res, next) {
     {
       var $ = cheerio.load(html);
       var data="";
+	  var description = $('meta[name="description"]');
+      var title = $('title');
+	  var name = $('meta[itemprop="name"]');
+	  var image = $('meta[itemprop="image"]');
+	  var ogtype = $('meta[property="og:type"]');
+	  var ogtitle = $('meta[property="og:title"]');
+	  var ogimage = $('meta[property="og:image"]');
+	  var twcard = $('meta[name="twitter:card"]');
+	  var twtitle = $('meta[name="twitter:title"]');
+	  var twdesc = $('meta[name="twitter:description"]');
       var header = $('head');
       var $ = cheerio.load(html);
       $('nav').filter(function () {
@@ -65,7 +68,7 @@ router.get('/', function(req, res, next) {
       $('#content').filter(function () {
         data += $(this);
       });
-      res.render('index',{data:data,header:header});
+      res.render('index',{data:data,header:header,title:title, description:description, name:name, image:image, ogtype:ogtype, ogtitle:ogtitle, ogimage:ogimage, twcard:twcard, twdesc:twdesc, twtitle:twtitle});
     }
     else {
       res.render('index',{data:'ERROR',title:'ERROR',header:""});
@@ -81,6 +84,16 @@ router.get('/:a', function(req, res, next) {
     { var $ = cheerio.load(html);
       var data="";
       var header = $('head');
+	  var description = $('meta[name="description"]');
+	  var name = $('meta[itemprop="name"]');
+	  var image = $('meta[itemprop="image"]');
+	  var ogtype = $('meta[property="og:type"]');
+	  var ogtitle = $('meta[property="og:title"]');
+	  var ogimage = $('meta[property="og:image"]');
+	  var twcard = $('meta[name="twitter:card"]');
+	  var twtitle = $('meta[name="twitter:title"]');
+	  var twdesc = $('meta[name="twitter:description"]');
+      var title = $('title');
       var $ = cheerio.load(html);
       $('nav').filter(function () {
         data += $(this);
@@ -89,7 +102,7 @@ router.get('/:a', function(req, res, next) {
         data += $(this);
 
       });
-      res.render('index',{data:data,header:header});
+      res.render('index',{data:data,header:header,title:title, description:description, name:name, image:image, ogtype:ogtype, ogtitle:ogtitle, ogimage:ogimage, twcard:twcard, twdesc:twdesc, twtitle:twtitle});
     }
   });
 });
@@ -102,6 +115,16 @@ router.get('/:a/:b', function(req, res, next) {
     { var $ = cheerio.load(html);
       var data="";
       var header = $('head');
+	  var description = $('meta[name="description"]');
+	  var name = $('meta[itemprop="name"]');
+	  var image = $('meta[itemprop="image"]');
+	  var ogtype = $('meta[property="og:type"]');
+	  var ogtitle = $('meta[property="og:title"]');
+	  var ogimage = $('meta[property="og:image"]');
+	  var twcard = $('meta[name="twitter:card"]');
+	  var twtitle = $('meta[name="twitter:title"]');
+	  var twdesc = $('meta[name="twitter:description"]');
+      var title = $('title');
       var $ = cheerio.load(html);
       $('nav').filter(function () {
         data += $(this);
@@ -110,7 +133,7 @@ router.get('/:a/:b', function(req, res, next) {
         data += $(this);
 
       });
-      res.render('index',{data:data,header:header});
+      res.render('index',{data:data,header:header,title:title, description:description, name:name, image:image, ogtype:ogtype, ogtitle:ogtitle, ogimage:ogimage, twcard:twcard, twdesc:twdesc, twtitle:twtitle});
     }
   });
 });
@@ -122,6 +145,16 @@ router.get('/:a/:b/:c', function(req, res, next) {
     if(!error)
     { var $ = cheerio.load(html);
       var data="";
+      var title = $('title');
+	  var description = $('meta[name="description"]');
+	  var name = $('meta[itemprop="name"]');
+	  var image = $('meta[itemprop="image"]');
+	  var ogtype = $('meta[property="og:type"]');
+	  var ogtitle = $('meta[property="og:title"]');
+	  var ogimage = $('meta[property="og:image"]');
+	  var twcard = $('meta[name="twitter:card"]');
+	  var twtitle = $('meta[name="twitter:title"]');
+	  var twdesc = $('meta[name="twitter:description"]');
       var header = $('head');
       var $ = cheerio.load(html);
       $('nav').filter(function () {
@@ -131,7 +164,7 @@ router.get('/:a/:b/:c', function(req, res, next) {
         data += $(this);
 
       });
-      res.render('index',{data:data,header:header});
+      res.render('index',{data:data,header:header,title:title, description:description, name:name, image:image, ogtype:ogtype, ogtitle:ogtitle, ogimage:ogimage, twcard:twcard, twdesc:twdesc, twtitle:twtitle});
     }
   });
 });
@@ -143,6 +176,16 @@ router.get('/:a/:b/:c/:d', function(req, res, next) {
     if(!error)
     { var $ = cheerio.load(html);
       var data="";
+	  var description = $('meta[name="description"]');
+	  var name = $('meta[itemprop="name"]');
+	  var image = $('meta[itemprop="image"]');
+	  var ogtype = $('meta[property="og:type"]');
+	  var ogtitle = $('meta[property="og:title"]');
+	  var ogimage = $('meta[property="og:image"]');
+	  var twcard = $('meta[name="twitter:card"]');
+	  var twtitle = $('meta[name="twitter:title"]');
+	  var twdesc = $('meta[name="twitter:description"]');
+      var title = $('title');
       var header = $('head');
       var $ = cheerio.load(html);
       $('nav').filter(function () {
@@ -152,78 +195,8 @@ router.get('/:a/:b/:c/:d', function(req, res, next) {
         data += $(this);
 
       });
-      res.render('index',{data:data,header:header});
+      res.render('index',{data:data,header:header,title:title, description:description, name:name, image:image, ogtype:ogtype, ogtitle:ogtitle, ogimage:ogimage, twcard:twcard, twdesc:twdesc, twtitle:twtitle});
     }
   });
 });
-// router.get('/:abc', function(req, res, next) {
-//   var page = req.params.abc;
-//   var q = req.param('q');
-//   if(typeof q == 'undefined')
-//     q="";
-//   else
-//      q="?q="+q;
-//   var url = 'http://nhentai.net/'+page+q;
-//
-//   request(url,function (error,response,html) {
-//     if(!error)
-//     {
-//       var data="";
-//       var $ = cheerio.load(html);
-//       $('nav').filter(function () {
-//         data += $(this);
-//       });
-//       $('#content').filter(function () {
-//        data += $(this);
-//         res.render('index',{data:data});
-//       });
-//     }
-//   });
-// });
-// router.get('/:a/:b', function(req, res, next) {
-//   var a = req.params.a;
-//   var b = req.params.b;
-//   var page = req.param('page');
-//   if(typeof page == 'undefined')
-//       page="";
-//   var url = 'http://nhentai.net/'+a+'/'+b+'?page='+page;
-//   request(url,function (error,response,html) {
-//     if(!error)
-//     {
-//       var data="";
-//       var $ = cheerio.load(html);
-//       $('nav').filter(function () {
-//         data += $(this);
-//       });
-//       $('#content').filter(function () {
-//         data += $(this);
-//         res.render('index',{data:data});
-//       });
-//     }
-//   });
-// });
-// router.get('/:a/:b/:c', function(req, res, next) {
-//   var a = req.params.a;
-//   var b = req.params.b;
-//   var c = req.params.c;
-//   var page = req.param('page');
-//   if(typeof page == 'undefined')
-//       page="";
-//   var url = 'http://nhentai.net/'+a+'/'+b+'/'+c+'?page='+page;
-//   request(url,function (error,response,html) {
-//     if(!error)
-//     {
-//       var data="";
-//       var $ = cheerio.load(html);
-//       $('nav').filter(function () {
-//         data += $(this);
-//       });
-//       $('#content').filter(function () {
-//         data += $(this);
-//         res.render('index',{data:data});
-//       });
-//     }
-//   });
-// });
-
 module.exports = router;
